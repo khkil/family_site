385fb85cd8eb9c659a148fa1755aad60cc171187
khkil <robot625@naver.com>
2024-01-23 09:09:18 +0900

    feat: initial commit

---
+++ api/src/main/java/com/example/googoos_api/GoogoosApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.googoos_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogoosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogoosApiApplication.class, args);
+	}
+
+}
