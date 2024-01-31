cac4a41c99fc4484de0e4b8212093bc7a6300914
khkil <robot625@naver.com>
2024-01-31 17:59:19 +0900

    chore: 레시피 목록 반복문 예외처리

--- googoos_site/pages/cooking/_components/CookingRecipe.tsx
+++ googoos_site/pages/cooking/_components/CookingRecipe.tsx
@@ -9,7 +9,7 @@ export default function CookingRecipe({ recipes }: Props): ReactElement {
   return (
     <div>
       <BlockTitle withBlock={false}>조리과정</BlockTitle>
-      {recipes.map(({ id, step, description, notice }: Recipe) => (
+      {recipes?.map(({ id, step, description, notice }: Recipe) => (
         <Card key={id} outline>
           {`${step}. ${description}`}
         </Card>
